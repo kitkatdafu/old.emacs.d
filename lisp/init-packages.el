43e5a7fbdfca2e544cf0007df06e56239ca52a6b
Yi Chen <mrr1vfe@gmail.com>
2019-07-28 16:18:30 -0500

    change on company-mode
    let company only work for prog-mode

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -171,7 +171,7 @@
 (require 'company)
 (require 'company-emacs-eclim)
 (company-emacs-eclim-setup)
-(global-company-mode t)
+(add-hook 'prog-mode-hook 'company-mode)
 (setq help-at-pt-display-when-idle t)
 (setq help-at-pt-timer-delay 0.1)
 (help-at-pt-set-timer)
