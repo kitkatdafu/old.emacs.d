b61b0ed381445ea73f670155d6b81aee45d89b59
Yi Chen <mrr1vfe@gmail.com>
2019-07-30 21:57:13 -0500

    keybinding change
    C-a is back to indentation
    M-a is move beginning of line

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -22,6 +22,16 @@
 (global-set-key (kbd "H-w") #'aya-create)
 (global-set-key (kbd "H-y") #'aya-expand)
 
+;; using C-n and C-p instead of M-n and M-p
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
+(global-set-key (kbd "C-a") 'back-to-indentation)
+(global-set-key (kbd "M-a") 'move-beginning-of-line)
+
 (provide 'init-keybindings.el)
 ;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
 (global-set-key (kbd "<f2>") 'open-init-file)
