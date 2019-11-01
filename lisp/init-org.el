b4839cec3ad794a9efcd3edef77f918941d288f1
rchen <mrr1vfe@gmail.com>
2019-10-31 19:56:00 -0500

    allow alphabet for list in org mode

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,7 +1,9 @@
 ;; org-mode语法高亮
 (require 'org)
 (setq org-src-fontify-natively t)
+(setq org-list-allow-alphabetical t)
 (set-language-environment "UTF-8")
 (add-hook 'org-mode-hook 'toggle-truncate-lines)
+
 
 (provide 'init-org)
