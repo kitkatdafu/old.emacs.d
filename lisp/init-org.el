b2c3f8b912b9b59e147cfd19949477b831063897
mrr1vfe <mrr1vfe@gmail.com>
2019-12-29 14:47:48 +0800

    add template for css

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -5,5 +5,10 @@
 (set-language-environment "UTF-8")
 (add-hook 'org-mode-hook 'toggle-truncate-lines)
 
+(add-to-list 'org-structure-template-alist '("N" "#+TITLE: 
+#+AUTHOR: Yi Chen
+#+EMAIL: ychen878@wisc.edu
+#+HTML_HEAD: <link rel=\"stylesheet\" type=\"text/css\" href=\"https://www.mrr1vfe.io/docs/org.css\"/>"))
+
 
 (provide 'init-org)
