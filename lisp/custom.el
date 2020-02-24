eadf6c0ea350491dd8cfd187f1704012826f43d3
Yi Chen <ychen878@wisc.edu>
2020-02-23 21:18:24 -0600

    Update custom.el

--- lisp/custom.el
+++ lisp/custom.el
@@ -6,8 +6,7 @@
  '(company-idle-delay 0.01)
  '(company-minimum-prefix-length 1)
  '(custom-safe-themes
-   (quote
-    ("e47c0abe03e0484ddadf2ae57d32b0f29f0b2ddfe7ec810bd6d558765d9a6a6c" default)))
+   '("e47c0abe03e0484ddadf2ae57d32b0f29f0b2ddfe7ec810bd6d558765d9a6a6c" default))
  '(lsp-ui-doc-use-webkit t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
