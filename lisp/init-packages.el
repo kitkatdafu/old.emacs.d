3db077e9cbe4767ffca3bf87f224a2c6fff5f286
Yi Chen <ychen878@wisc.edu>
2020-04-15 15:00:45 -0500

    Update init-packages.el

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -150,12 +150,6 @@
 ;; setup treemacs
 (setq treemacs-width 30)
 
-;; bash
-(use-package lsp-mode
-  :commands lsp
-  :hook
-  (sh-mode . lsp))
-
 ;; setup js2-mode
 ;; (setq auto-mode-alist
 ;;      (append
