acfe2167a86e1d5ed80c90625cced59ed94e509a
Yi Chen <ychen878@wisc.edu>
2020-04-15 13:34:47 -0500

    Update init-packages.el

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -37,12 +37,12 @@
 	       expand-region
 	       iedit
 	       flycheck
-               js2-mode
+               ;; js2-mode
 	       web-mode
-               nodejs-repl
+               ;; nodejs-repl
                exec-path-from-shell
 	       nyan-mode
-	       js2-refactor
+	       ;  js2-refactor
 	       yasnippet
 	       auto-yasnippet
 	       lsp-mode
@@ -93,24 +93,24 @@
 (setq enable-recursive-minibuffers t)
 
 ;; setup imenu
-(defun js2-imenu-make-index ()
-      (interactive)
-      (save-excursion
+;; (defun js2-imenu-make-index ()
+;;      (interactive)
+;;      (save-excursion
 	;; (setq imenu-generic-expression '((nil "describe\\(\"\\(.+\\)\"" 1)))
-	(imenu--generic-function '(("describe" "\\s-*describe\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-				   ("it" "\\s-*it\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-				   ("test" "\\s-*test\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-				   ("before" "\\s-*before\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-				   ("after" "\\s-*after\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-				   ("Function" "function[ \t]+\\([a-zA-Z0-9_$.]+\\)[ \t]*(" 1)
-				   ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
-				   ("Function" "^var[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
-				   ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*()[ \t]*{" 1)
-				   ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*:[ \t]*function[ \t]*(" 1)
-				   ("Task" "[. \t]task([ \t]*['\"]\\([^'\"]+\\)" 1)))))
-(add-hook 'js2-mode-hook
-	      (lambda ()
-		(setq imenu-create-index-function 'js2-imenu-make-index)))
+;;	(imenu--generic-function '(("describe" "\\s-*describe\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+;;				   ("it" "\\s-*it\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+;;				   ("test" "\\s-*test\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+;;				   ("before" "\\s-*before\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+;;				   ("after" "\\s-*after\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+;;				   ("Function" "function[ \t]+\\([a-zA-Z0-9_$.]+\\)[ \t]*(" 1)
+;;				   ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
+;;				   ("Function" "^var[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
+;;				   ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*()[ \t]*{" 1)
+;;				   ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*:[ \t]*function[ \t]*(" 1)
+;;				   ("Task" "[. \t]task([ \t]*['\"]\\([^'\"]+\\)" 1)))))
+;; (add-hook 'js2-mode-hook
+;;	      (lambda ()
+;;		(setq imenu-create-index-function 'js2-imenu-make-index)))
 
 
 ;; setup dwim 
@@ -128,7 +128,7 @@
   (call-interactively 'occur))
 
 ;; setup nodejs-repl
-(require 'nodejs-repl)
+;; (require 'nodejs-repl)
 
 ;; setup lsp-mode
 (require 'lsp-mode)
@@ -153,14 +153,14 @@
 
 
 ;; setup js2-mode
-(setq auto-mode-alist
-      (append
-       '(("\\.js\\'" . js2-mode))
-       '(("\\.html\\'" . web-mode))
-       auto-mode-alist))
+;; (setq auto-mode-alist
+;;      (append
+;;       '(("\\.js\\'" . js2-mode))
+;;       '(("\\.html\\'" . web-mode))
+;;       auto-mode-alist))
 
 ;; setup js2-refactor
-(add-hook 'js2-mode-hook #'js2-refactor-mode)
+;;(add-hook 'js2-mode-hook #'js2-refactor-mode)
 
 ;; setup smartparens
 (smartparens-global-mode t)
