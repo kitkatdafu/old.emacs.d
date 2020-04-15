dafff87ffb41f2278ca91942ca097d40b3804791
Yi Chen <ychen878@wisc.edu>
2020-04-15 13:37:36 -0500

    Update init.el

--- init.el
+++ init.el
@@ -14,7 +14,6 @@
 (require 'init-better-defaults)
 (require 'init-org)
 (require 'init-keybindings)
-(require 'quick-print)
 
 (setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
 (load-file custom-file)
