fe356649c9848b01cb71ba254bbc862ebd661416
mrr1vfe <mrr1vfe@gmail.com>
2020-08-23 21:01:46 -0500

    Update init-packages.el

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -84,6 +84,26 @@
 (require 'evil)
 (evil-mode 1)
 
+;; web-mode
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+(add-hook 'local-write-file-hooks
+            (lambda ()
+               (delete-trailing-whitespace)
+               nil))
+
 ;; org-ref
 ;; (require 'org-ref)
 
