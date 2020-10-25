8739a85f0db34405689d4446cb711dcf389c9775
Yi Chen <mrr1vfe@gmail.com>
2020-10-25 08:51:18 -0500

    Update init-packages.el

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -1,27 +1,16 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;;; package --- init-packages
+;;; Commentary:
+;;; Install and setup packages here
+;;; Code:
 (when (>= emacs-major-version 24)
      (require 'package)
      (package-initialize))
 
-;; (setq am-i-in-china nil)
-;; (if am-i-in-china
-;;     (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-;; 			     ("melpa" . "http://elpa.emacs-china.org/melpa/")))
-;;     (setq package-archives '(
-;; 			      ("melpa-stable" . "http://stable.melpa.org/packages/")
-;; 			      ("gnu" . "http://elpa.gnu.org/packages/")
-;; 			      ("elpa" . "http://melpa.org/packages/")
-;; 			      ))
-;; )
-
-;; cl - Common Lisp Extension
-(require 'cl)
-
-;; (setenv "PATH" (concat "/usr/local/smlnj/bin:" (getenv "PATH")))
-;; (setq exec-path (cons "/usr/local/smlnj/bin"  exec-path))
+(setq package-archives '(
+			 ("melpa-stable" . "http://stable.melpa.org/packages/")
+			 ("gnu" . "http://elpa.gnu.org/packages/")
+			 ("elpa" . "http://melpa.org/packages/")
+			 ))
 
 ;; Add Packages
 (defvar ddy/packages '(
@@ -35,42 +24,44 @@
 	       expand-region
 	       iedit
 	       flycheck
-	       web-mode
                exec-path-from-shell
 	       nyan-mode
 	       yasnippet
 	       auto-yasnippet
-	       lsp-mode
 	       company-lsp
 	       company-box
+	       lsp-mode
 	       lsp-treemacs
 	       lsp-ui
 	       lsp-python-ms
 	       lsp-haskell
-	       treemacs
+	       js2-mode
+	       nodejs-repl
+	       web-mode
 	       htmlize
+	       treemacs
+	       treemacs-icons-dired
+	       treemacs-magit
 	       haskell-mode
 	       flyspell-popup
 	       rust-mode
 	       all-the-icons
 	       magit
-	       treemacs-magit
-	       treemacs-icons-dired
 	       tuareg
 	       org-ref
 	       ox-twbs
 	       doom-themes
 	       evil
 	       ivy-bibtex
-	       ) "Default packages")
+	       ) "Default packages.")
 
 (setq package-selected-packages ddy/packages)
 
 (defun ddy/packages-installed-p ()
-  
-  (loop for pkg in ddy/packages
-	when (not (package-installed-p pkg)) do (return nil)
-	finally (return t)))
+  "Install packages."
+  (cl-loop for pkg in ddy/packages
+	when (not (package-installed-p pkg)) do (cl-return nil)
+	finally (cl-return t)))
 
 (unless (ddy/packages-installed-p)
   (message "%s" "Refreshing package database...")
@@ -87,7 +78,6 @@
 (evil-mode 1)
 
 ;; web-mode
-
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
@@ -99,13 +89,12 @@
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
 )
-(add-hook 'web-mode-hook  'my-web-mode-hook)
+(add-hook 'web-mode-hook 'my-web-mode-hook)
 
-(add-hook 'local-write-file-hooks
+(add-hook 'write-file-functions
             (lambda ()
                (delete-trailing-whitespace)
                nil))
-
 
 ;; setup swiper/ivy
 (ivy-mode 1)
@@ -113,24 +102,25 @@
 (setq enable-recursive-minibuffers t)
 
 ;; setup imenu
-;; (defun js2-imenu-make-index ()
-;;      (interactive)
-;;      (save-excursion
-	;; (setq imenu-generic-expression '((nil "describe\\(\"\\(.+\\)\"" 1)))
-;;	(imenu--generic-function '(("describe" "\\s-*describe\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-;;				   ("it" "\\s-*it\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-;;				   ("test" "\\s-*test\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-;;				   ("before" "\\s-*before\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-;;				   ("after" "\\s-*after\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-;;				   ("Function" "function[ \t]+\\([a-zA-Z0-9_$.]+\\)[ \t]*(" 1)
-;;				   ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
-;;				   ("Function" "^var[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
-;;				   ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*()[ \t]*{" 1)
-;;				   ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*:[ \t]*function[ \t]*(" 1)
-;;				   ("Task" "[. \t]task([ \t]*['\"]\\([^'\"]+\\)" 1)))))
-;; (add-hook 'js2-mode-hook
-;;	      (lambda ()
-;;		(setq imenu-create-index-function 'js2-imenu-make-index)))
+(defun js2-imenu-make-index ()
+  "Function for setting js2-imenu."
+  (interactive)
+  (save-excursion
+    (setq imenu-generic-expression '((nil "describe\\(\"\\(.+\\)\"" 1)))
+    (imenu--generic-function '(("describe" "\\s-*describe\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+			       ("it" "\\s-*it\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+			       ("test" "\\s-*test\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+			       ("before" "\\s-*before\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+			       ("after" "\\s-*after\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+			       ("Function" "function[ \t]+\\([a-zA-Z0-9_$.]+\\)[ \t]*(" 1)
+			       ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
+			       ("Function" "^var[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
+			       ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*()[ \t]*{" 1)
+			       ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*:[ \t]*function[ \t]*(" 1)
+			       ("Task" "[. \t]task([ \t]*['\"]\\([^'\"]+\\)" 1)))))
+(add-hook 'js2-mode-hook
+	  (lambda ()
+	    (setq imenu-create-index-function 'js2-imenu-make-index)))
 
 
 ;; setup dwim 
@@ -148,13 +138,12 @@
   (call-interactively 'occur))
 
 ;; setup nodejs-repl
-;; (require 'nodejs-repl)
+(require 'nodejs-repl)
 
 ;; setup lsp-mode
 (require 'lsp-mode)
 (setq lsp-prefer-flymake nil) ;; prefer flycheck
 (setq lsp-enable-indentation t)
-
 
 ;; setup lsp-ui
 (require 'lsp-ui)
@@ -177,19 +166,21 @@
 (setq company-lsp-async 1)
 (setq company-lsp-cache-candidates t)
 
+;; all the icons
+(require 'all-the-icons)
 
 ;; setup treemacs
 (setq treemacs-width 30)
 
 ;; setup js2-mode
-;; (setq auto-mode-alist
-;;      (append
-;;       '(("\\.js\\'" . js2-mode))
-;;       '(("\\.html\\'" . web-mode))
-;;       auto-mode-alist))
+(setq auto-mode-alist
+     (append
+      '(("\\.js\\'" . js2-mode))
+      '(("\\.html\\'" . web-mode))
+      auto-mode-alist))
 
 ;; setup js2-refactor
-;;(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
 
 ;; setup smartparens
 (smartparens-global-mode t)
@@ -205,8 +196,7 @@
 (require 'all-the-icons)
 
 ;; setup doom themes
-(require 'doom-themes
-)
+(require 'doom-themes)
 ;; Global settings (defaults)
 (setq doom-themes-enable-bold t
       doom-themes-enable-italic t)
@@ -222,8 +212,8 @@
 ;; setup company
 (require 'company)
 (add-hook 'prog-mode-hook 'company-mode)
-(setq company-idle-delay 0.125)
-(setq company-minimum-prefix-length 2)
+(setq company-idle-delay 0)
+(setq company-minimum-prefix-length 1)
 (setq help-at-pt-display-when-idle t)
 (help-at-pt-set-timer)
 (require 'company-box)
@@ -236,8 +226,6 @@
 ;; setup flyspell
 (flyspell-mode t)
 (setq flyspell-issue-message-flag nil) ;; improve performance
-(add-to-list 'ispell-skip-region-alist '("^#+BEGIN_SRC" . "^#+END_SRC"))
-(add-hook 'org-mode-hook 'flyspell-mode)
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 (define-key flyspell-mode-map (kbd "C-;") #'flyspell-popup-correct)
 
@@ -258,24 +246,9 @@
 (add-hook 'prog-mode-hook #'yas-minor-mode)
 
 ;; setup rust mode
-;; (add-hook 'rust-mode-hook
-;;          (lambda () (setq indent-tabs-mode nil)))
-;; (setq rust-format-on-save t)
-
-
-;; set bibtex
-(setq bibtex-completion-library-path '("~/Documents/bibliography/bibtex-pdfs/"))
-
-;; org-ref
-(require 'org-ref)
-(setq reftex-default-bibliography '("~/Documents/bibliography/references.bib"))
-(setq org-ref-bibliography-notes "~/Documents/bibliography/notes.org"
-      org-ref-default-bibliography '("~/Documents/bibliography/references.bib")
-      org-ref-pdf-directory "~/Documents/bibliography/bibtex-pdfs/")
-(setq org-latex-pdf-process
-      '("pdflatex -interaction nonstopmode -output-directory %o %f"
-	"bibtex %b"
-	"pdflatex -interaction nonstopmode -output-directory %o %f"
-	"pdflatex -interaction nonstopmode -output-directory %o %f"))
+(add-hook 'rust-mode-hook
+         (lambda () (setq indent-tabs-mode nil)))
+(setq rust-format-on-save t)
 
 (provide 'init-packages)
+;;; init-packages.el ends here
