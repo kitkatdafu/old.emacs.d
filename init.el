d87a5a3cb1054931a74a20e0b7fb8d1ba37f7cd7
Yi Chen <mrr1vfe@gmail.com>
2020-10-25 08:51:08 -0500

    Update init.el

--- init.el
+++ init.el
@@ -1,18 +1,16 @@
+;;; package --- init
+;;; Commentary:
+;;; Hello World, this is the beginning
+;;; Code:
 (add-to-list 'load-path "~/.emacs.d/lisp")
-
-;; 快速打开配置文件
-(defun open-init-file()
-  (interactive)
-  (find-file "~/.emacs.d/init.el"))
 
 ;; circumvent signature check
 (setq package-check-signature nil)
 
-;; (require 'check-location)
 (require 'init-packages)
 (require 'init-ui)
 (require 'init-better-defaults)
 (require 'init-org)
 (require 'init-keybindings)
-;; (require 'beginner-guide)
 ;; (require 'easypanol)
+;;; init.el ends here
