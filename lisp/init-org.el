8bf00950842377ae38c3458e22b08e7c68eb28c9
Yi Chen <mrr1vfe@gmail.com>
2021-01-16 09:05:53 -0600

    Update init-org.el

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -29,5 +29,33 @@
 	  "pdflatex -interaction nonstopmode -output-directory %o %f"))
   )
 
+(use-package ox-publish
+  :defer t
+  :init
+  (setq org-publish-project-alist
+      '(
+
+	("org-notes"
+	 :auto-sitemap t
+	 :index-filename "sitemap.org"
+	 :index-title ""
+	 :base-directory "~/org/"
+	 :base-extension "org"
+	 :publishing-directory "~/public_html/"
+	 :recursive t
+	 :publishing-function org-html-publish-to-html
+	 :headline-levels 4             ; Just the default for this project.
+	 :auto-preamble t
+	 )
+	("org-static"
+	 :base-directory "~/org/"
+	 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+	 :publishing-directory "~/public_html/"
+	 :recursive t
+	 :publishing-function org-publish-attachment
+	 )
+	("org" :components ("org-notes" "org-static"))
+	))
+  )
 (provide 'init-org)
 ;;; init-org.el ends here
