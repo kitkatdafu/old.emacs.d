e9da06d1b96dfa354f32eb76f20a9e627cfce05c
Yi Chen <mrr1vfe@gmail.com>
2021-01-17 06:11:54 -0600

    Update init-better-defaults.el

--- lisp/init-better-defaults.el
+++ lisp/init-better-defaults.el
@@ -93,6 +93,10 @@
 (add-hook 'c++-mode-hook 'my-c++-mode-hook)
 (add-hook 'c-mode-hook 'my-c++-mode-hook)
 
+(add-hook 'java-mode-hook (lambda ()
+			    (setq c-basic-offset 4
+				  tab-width 4
+				  indent-tabs-mode t)))
 
 ;; term mouse
 (unless window-system
