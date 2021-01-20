c653c32cc3e9050e7d6013f24d2d31d8be4a758c
Reid Chen <mrr1vfe@gmail.com>
2021-01-20 08:22:47 -0600

    Create init-ui.el

---
+++ lisp/init-ui.el
@@ -0,0 +1,84 @@
+;;; package --- init-ui
+;;; Commentary:
+;;; Setup user interface, font, etc.
+;;; Code:
+;; turn off tool bar
+(tool-bar-mode -1)
+;; turn off scroll bar
+(when (display-graphic-p)
+  (scroll-bar-mode -1))
+;; display line number
+(global-linum-mode t)
+;; setup font
+(defvar editor-font "Iosevka-18")
+(set-face-attribute 'default nil :font editor-font)
+(set-frame-font editor-font nil t)
+;; setup transparent title bar
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
+;; setup all the icons
+(use-package all-the-icons)
+;; setup nyan-mode cat
+(use-package nyan-mode
+  :config
+  (nyan-mode)
+  (nyan-start-animation)
+  (setq nyan-wavy-trail t)
+  (setq nyan-animate-nyancat t)
+  )
+;; setup doom themes
+(use-package doom-themes
+  :config
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t)
+  (load-theme 'doom-monokai-pro t)
+  ;; Corrects and improves org-mode's native fontification.
+  (doom-themes-org-config))
+;; setup doom modeline
+(use-package doom-modeline
+  :ensure t
+  :init
+  (doom-modeline-mode 1))
+
+;; setup ligatures
+(let ((ligatures `((?-  . ,(regexp-opt '("-|" "-~" "---" "-<<" "-<" "--" "->" "->>" "-->")))
+                   (?/  . ,(regexp-opt '("/**" "/*" "///" "/=" "/==" "/>" "//")))
+                   (?*  . ,(regexp-opt '("*>" "***" "*/")))
+                   (?<  . ,(regexp-opt '("<-" "<<-" "<=>" "<=" "<|" "<||" "<|||::=" "<|>" "<:" "<>" "<-<"
+                                         "<<<" "<==" "<<=" "<=<" "<==>" "<-|" "<<" "<~>" "<=|" "<~~" "<~"
+                                         "<$>" "<$" "<+>" "<+" "</>" "</" "<*" "<*>" "<->" "<!--")))
+                   (?:  . ,(regexp-opt '(":>" ":<" ":::" "::" ":?" ":?>" ":=")))
+                   (?=  . ,(regexp-opt '("=>>" "==>" "=/=" "=!=" "=>" "===" "=:=" "==")))
+                   (?!  . ,(regexp-opt '("!==" "!!" "!=")))
+                   (?>  . ,(regexp-opt '(">]" ">:" ">>-" ">>=" ">=>" ">>>" ">-" ">=")))
+                   (?&  . ,(regexp-opt '("&&&" "&&")))
+                   (?|  . ,(regexp-opt '("|||>" "||>" "|>" "|]" "|}" "|=>" "|->" "|=" "||-" "|-" "||=" "||")))
+                   (?.  . ,(regexp-opt '(".." ".?" ".=" ".-" "..<" "...")))
+                   (?+  . ,(regexp-opt '("+++" "+>" "++")))
+                   (?\[ . ,(regexp-opt '("[||]" "[<" "[|")))
+                   (?\{ . ,(regexp-opt '("{|")))
+                   (?\? . ,(regexp-opt '("??" "?." "?=" "?:")))
+                   (?#  . ,(regexp-opt '("####" "###" "#[" "#{" "#=" "#!" "#:" "#_(" "#_" "#?" "#(" "##")))
+                   (?\; . ,(regexp-opt '(";;")))
+                   (?_  . ,(regexp-opt '("_|_" "__")))
+                   (?\\ . ,(regexp-opt '("\\" "\\/")))
+                   (?~  . ,(regexp-opt '("~~" "~~>" "~>" "~=" "~-" "~@")))
+                   (?$  . ,(regexp-opt '("$>")))
+                   (?^  . ,(regexp-opt '("^=")))
+                   (?\] . ,(regexp-opt '("]#"))))))
+  (dolist (char-regexp ligatures)
+    (set-char-table-range composition-function-table (car char-regexp)
+                          `([,(cdr char-regexp) 0 font-shape-gstring]))))
+
+(use-package composite
+  :hook (prog-mode . auto-composition-mode)
+  :init (global-auto-composition-mode -1))
+
+(setq helm-display-header-line nil)
+(helm-autoresize-mode 1)
+(setq helm-autoresize-max-height 20)
+(setq helm-autoresize-min-height 20)
+(setq helm-split-window-in-side-p t)
+
+(provide 'init-ui)
+;;; init-ui.el ends here
